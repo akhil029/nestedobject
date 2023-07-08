069997af6dc5d9ef2d44ca34cef5f3771ccf8e35
akhil029 <98713091+akhil029@users.noreply.github.com>
2023-07-09 01:07:57 +0530

    nestedobject

---
+++ NestedObject.ps1
@@ -0,0 +1,36 @@
+﻿function GetNestedValue {
+    param (
+        [Parameter(Mandatory = $true)]
+        [object]$Object,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Key
+    )
+
+    $keys = $Key -split '/'
+    $value = $Object
+
+    try {
+        foreach ($k in $keys) {
+            $value = $value.$k
+        }
+    }
+    catch {
+        $value = $null
+    }
+
+    return $value
+}
+
+
+$Epmloyee = @{
+    "Department" = @{
+        "DevOps" = @{
+            "Team" = "Microsoft Business"
+        }
+    }
+}
+$key1 = "Department/DevOps/Team"
+$result1 = GetNestedValue -Object $Epmloyee -Key $key1
+Write-Output $Key1
+Write-Output $result1
